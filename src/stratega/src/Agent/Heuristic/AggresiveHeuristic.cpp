e1417fc97a0790d9788c145d02dc4ce2e0721416
George Long <george@longhouse.dev>
2023-02-05 14:52:45 +0000

    Added testmaps + new heuristic

--- src/stratega/src/Agent/Heuristic/AggresiveHeuristic.cpp
+++ src/stratega/src/Agent/Heuristic/AggresiveHeuristic.cpp
@@ -8,36 +8,44 @@ namespace SGA {
 double AggresiveHeuristic::evaluateGameState(
    const ForwardModel& /*forwardModel*/, GameState& gameState, int playerID)
 {
-   // Find Strongest Unit
-   // Make Strongest Unit Attack King
-   double score = 0;
-   if (playerID != gameState.getCurrentTBSPlayer()) {
-      return -1000000000000;
-   }
+    // Variables
+    std::map< int, Vector2f > positions;
+    std::list<Vector2f > supportPositions;
+    double totalDistance = 0;
+    int kingX = 0;
+    int kingY = 0;
+    double score = 0;
 
-   if(gameState.isGameOver()) {
-   if(gameState.getWinnerID() == playerID)
-      return 10000000000000;  // since this score should be minimized we need to deduct points
-                                 // for winning
-   else
-      return -1000000000000;
-   }
+    // Modifiers
+    std::set< int > opponentEntities = std::set< int >();
+    std::set< int > playerEntities = std::set< int >();
+    double playerArmyHealth = 0;
+    double opponentArmyHealth = 0;
+    double playerKingHP = 0;
+    double opponentKingHP = 0;
+    double meanDistance = 0;
+    double supportUnitsinRange = 0;
 
-   std::map< int, Vector2f > positions;
-   std::set< int > opponentEntities = std::set< int >();
-   std::set< int > playerEntities = std::set< int >();
+    // Weights
+    double opponentArmySizeMOD = 1;
+    double playerArmySizeMOD = 1;
+    double playerArmyHealthMOD = 0.1;
+    double opponentArmyHealthMOD = 0.1;
+    double playerKingHPMOD = 1;
+    double opponentKingHPMOD = 1;
+    double meanDistanceMOD = 1;
+    double supportUnitsinRangeMOD = 1;
 
-   double playerArmyHealth = 0;
-   double enemyArmyHealth = 0;
-   double playerKingHP = 0;
-   double enemyKingHP = 0;
-   int kingX = 0;
-   int kingY = 0;
+    // If playerID is not the current player
+    if (playerID != gameState.getCurrentTBSPlayer()) {
+        return -1000000000000;
+    }
 
-   double totalDistance = 0;
-   double meanDistance = 0;
-
-   int bob = gameState.getCurrentTBSPlayer();
+    // Win check
+    if(gameState.isGameOver()) {
+        if(gameState.getWinnerID() == playerID) return 10000000000000;
+        else return -1000000000000;
+    }
 
    // Register Entities
    for(const auto& entity : gameState.getEntities()) {
@@ -51,10 +59,14 @@ double AggresiveHeuristic::evaluateGameState(
          if(entityType.getName() == "King") {
             kingX = static_cast< int >(entity.x());
             kingY = static_cast< int >(entity.y());
-            enemyKingHP = entity.getParameter("Health");
+            opponentKingHP = entity.getParameter("Health");
          } 
+         // Support Units
+         else if(entityType.getName() == "Druid" || entityType.getName() == "Engineer") {
+            supportPositions.push_front(entity.getPosition());
+         }
          else {
-            enemyArmyHealth += entity.getParameter("Health");
+            opponentArmyHealth += entity.getParameter("Health");
          }
 
       } else if(entity.getOwnerID() == gameState.getCurrentTBSPlayer()) {
@@ -71,34 +83,43 @@ double AggresiveHeuristic::evaluateGameState(
 
    // Gets the total distance of player units (apart from king!) and enemy king
    for(const auto& entity : playerEntities) {
-      if((gameState.getGameInfo()
-             ->getEntityType(gameState.getEntity(entity)->getEntityTypeID())
-             .getName()
-          != "King")) {
-         double deltaX = pow((positions[entity].x - kingX), 2);
-         double deltaY = pow((positions[entity].y - kingY), 2);
-         double distance = (abs(positions[entity].x - kingX) + abs(positions[entity].y - kingY)) - gameState.getEntity(entity)->getParameter("AttackRange");
-         totalDistance += distance;
-         //totalDistance += abs(positions[entity].x - kingX) + abs(positions[entity].y - kingY);
+
+      std::string name = gameState.getGameInfo()
+        ->getEntityType(gameState.getEntity(entity)->getEntityTypeID())
+        .getName();
+
+      if (name != "Druid" || name != "Engineer") {
+         // Check how many units are in range of a support
+         for(Vector2f supportPosition : supportPositions) {
+            double distance = supportPosition.manhattanDistance(positions[entity]);
+            if(distance <= 3)
+               supportUnitsinRange += 1;
+            break;
+         }
+      }
+
+      // Checks the distance of offensive units from the opposing king
+      if(name != "King" || name != "Druid" || name != "Engineer") {
+
+          // Manhattan Method for distance
+            double distance = (abs(positions[entity].x - kingX) + abs(positions[entity].y - kingY)) - gameState.getEntity(entity)->getParameter("AttackRange");
+            totalDistance += distance;
       }
    }
 
-   //meanDistance = totalDistance / static_cast< double >(playerEntities.size() - 1);
-   //score -= meanDistance * 100;
-   score -= totalDistance;
+   // Negative modifiers 
+   score -= meanDistance * meanDistanceMOD;
+   score -= opponentKingHP * opponentKingHPMOD;
+   score -= opponentArmyHealth * opponentArmyHealthMOD;
+   score -= opponentEntities.size() * opponentArmySizeMOD;
 
-   score -= enemyKingHP * 2;
-   score -= enemyArmyHealth;
-   score += playerArmyHealth;
-   score += playerKingHP;
+   // Positive modifiers
+   score += playerArmyHealth * playerArmyHealthMOD;
+   score += playerKingHP * playerKingHPMOD;
+   score += playerEntities.size() * playerArmySizeMOD;
+   score += supportUnitsinRange * supportUnitsinRangeMOD;
 
-   score -= opponentEntities.size();
-   score += playerEntities.size();
-
-
-   //score -= enemyKingHP;
-
-   return score;
+   return score *= 1;
 }
 AggresiveHeuristic::AggresiveHeuristic(GameState state)
 {
